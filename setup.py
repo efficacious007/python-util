3f2e127f5b4a1d64d88b778fa8df782a9b5e6d51
uday dosajh <dosajhuday@gmail.com>
2020-06-11 22:01:54 +0530

    packaging module

--- setup.py
+++ setup.py
@@ -4,16 +4,10 @@ with open("README.md", "r") as fh:
 setup(
      name='sqlite-util',  
      version='0.1',
-     scripts=['sqlite-util'] ,
      author="Uday Dosajh",
      author_email="dosajhuday@gmail.com",
      description="sqlite utility",
      long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/efficacious007/python-util",
-     packages=find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "Operating System :: OS Independent"
-     ],
+     long_description_content_type="text/markdown",
+     packages=find_packages()
  )
